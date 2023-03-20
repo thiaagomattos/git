363dc33f723d0f668f9f663b3e20352915efd18f
Thiago Mattos <euthiagomattos@gmail.com>
2023-03-20 10:23:21 -0300

    Criando arquivo funcoes.js com lista de cursos

---
+++ funcoes.js
@@ -0,0 +1,20 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// 1) declara a função
+                      //string
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// 2) executa a função
+
+// imprimeTexto(soma()"
+// imprimeTexto("outro texto")
+
+function soma(){
+    return 2 + 2;
+}
+
+// console.log(soma())
